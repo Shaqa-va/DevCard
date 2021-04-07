7fac2bbe97ea1783d132ac9ef7d2f46d1515249a
Shaqa-va <shaqavaziri1@gmail.com>
2021-04-07 12:26:08 +0430

    Contact add

--- DevCard_MVC/Controllers/HomeController.cs
+++ DevCard_MVC/Controllers/HomeController.cs
@@ -1,11 +1,8 @@
 ﻿using DevCard_MVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace DevCard_MVC.Controllers
 {
@@ -18,6 +15,11 @@ namespace DevCard_MVC.Controllers
             _logger = logger;
         }
 
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
